ef5cff19e33bd64e7d3df280bdec0d2e8c128a45
Philipp Thiel <taronyu@users.noreply.github.com>
2021-09-16 13:55:03 +0200

    Replace flask with base http server

--- bmvd/webserver.py
+++ bmvd/webserver.py
@@ -1,16 +1,33 @@
 import argparse
-from flask import Flask, json
-
-api = Flask(__name__)
+import json
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
 data = dict()
 data["V"] = "12800"
 data["I"] = "-1000"
 
 
-@api.route("/bmv600s", methods=["GET"])
-def get_bmv600s_status():
-    return json.dumps(data)
+class BmvRequestHandler(BaseHTTPRequestHandler):
+    server_version = "bmvd/0.1"
+
+    def do_GET(self):
+        if self.path == "/":
+            # Redirect to battery monitor status page
+            self.send_response(301)
+            self.send_header("Location", "/bmv600s")
+            self.end_headers()
+        elif self.path == "/bmv600s":
+            self._send_data()
+        else:
+            self.send_error(404, message="Path not found")
+
+    def _send_data(self):
+        self.send_response(200)
+        self.send_header("Content-type", "application/json")
+        self.end_headers()
+
+        dump = json.dumps(data)
+        self.wfile.write(dump.encode("utf-8"))
 
 
 def main():
@@ -18,7 +35,14 @@ def main():
     ap.add_argument("-p", "--port", metavar="PORT", type=int, default=7070,
                     help="server port to listen on")
     args = ap.parse_args()
-    api.run(host="0.0.0.0", port=args.port)
+
+    print("Starting HTTP server on port {0}".format(args.port))
+    server = HTTPServer(("", args.port), BmvRequestHandler)
+    try:
+        server.serve_forever()
+    except KeyboardInterrupt:
+        print("Server shutting down")
+        pass
 
 
 if __name__ == "__main__":
